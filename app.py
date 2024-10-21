848b6fd0dc85e63ea440560b4b1b637016638c71
Brian West <brian@signalwire.com>
2024-10-21 17:00:26 -0500

    update

--- app.py
+++ app.py
@@ -333,4 +333,4 @@ def serve_zendesk_html():
         return jsonify({"error": "Failed to serve zendesk.html"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(host="0.0.0.0", port=5001, debug=True)
